44c4bb8d0a9dd5157c09b2ba938ff31030c4a2b4
Vicente Penades <megaz0ne23@hotmail.com>
2017-05-18 22:57:30 +0200

    Added some basic tests

---
+++ src/Epsylon.UberFactory.Core.Tests/Tests.Pipeline.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Epsylon.UberFactory
+{
+    [TestClass]
+    public class PipelineTests
+    {        
+        [TestMethod]        
+        public void BasicPipelineTest()
+        {
+            var pipeline = TestPipelinesFactory.CreateBasicPipeline();
+            var result = (int)_Evaluate(pipeline, "Root");
+            Assert.AreEqual(12, result);
+        }
+
+        [TestMethod]
+        public void TreePipelineTest()
+        {
+            var pipeline = TestPipelinesFactory.CreateTreePipeline();
+            var result = (int)_Evaluate(pipeline,"Root");
+            Assert.AreEqual(24, result);
+        }
+
+        private static Object _Evaluate(ProjectDOM.Pipeline pipeline, string configuration)
+        {
+            // create a pipeline evaluator
+
+            var evaluator = PipelineEvaluator.CreatePipelineInstance(pipeline, id => null, TestFiltersFactory.CreateInstance, PipelineEvaluator.Monitor.Empty);
+            evaluator.Setup(BuildContext.Create(configuration, new PathString("")));
+
+            // run evaluation
+
+            // note we're using the secondary evaluator instead of the primary one, to prevent creating the target directory.
+            return (int)evaluator.Evaluate(pipeline.RootIdentifier);
+        }
+    }
+
+
+    static class TestPipelinesFactory
+    {
+        /// <summary>
+        /// Create a simple pipeline, with a single node that returns the sum of two values
+        /// </summary>
+        public static ProjectDOM.Pipeline CreateBasicPipeline()
+        {
+            // create a pipeline
+            var pipeline = new ProjectDOM.Pipeline();
+
+            // create a node and set it as root of the pipeline
+            var nodeId = pipeline.AddNode("TestFilter1");
+            pipeline.RootIdentifier = nodeId;
+
+            // set node properties for "Root" Configuration            
+            var nodeProps = pipeline.GetNode(nodeId).GetPropertiesForConfiguration("Root");
+            nodeProps.SetValue("Value1", "5");
+            nodeProps.SetValue("Value2", "7");
+
+            return pipeline;
+        }
+
+        /// <summary>
+        /// Create a complex pipeline, with a root node and two leaf nodes
+        /// </summary>        
+        public static ProjectDOM.Pipeline CreateTreePipeline()
+        {
+            // create a pipeline
+            var pipeline = new ProjectDOM.Pipeline();
+
+            // create root node and set it as root of the pipeline
+            var nodeId = pipeline.AddNode("TestFilter2");
+            pipeline.RootIdentifier = nodeId;
+
+            // create leaf nodes
+            var node2Id = pipeline.AddNode("TestFilter1");
+            var node3Id = pipeline.AddNode("TestFilter1");
+
+            // set leaf node properties
+            var nodeProps = pipeline.GetNode(node2Id).GetPropertiesForConfiguration("Root");
+            nodeProps.SetValue("Value1", "5");
+            nodeProps.SetValue("Value2", "7");
+
+            // set leaf node properties
+            nodeProps = pipeline.GetNode(node3Id).GetPropertiesForConfiguration("Root");
+            nodeProps.SetValue("Value1", "5");
+            nodeProps.SetValue("Value2", "7");
+
+            // set root node properties
+            nodeProps = pipeline.GetNode(nodeId).GetPropertiesForConfiguration("Root");
+            nodeProps.SetNodeIds("Value1", node2Id);
+            nodeProps.SetNodeIds("Value2", node3Id);
+
+            return pipeline;
+        }
+    }
+
+
+    static class TestFiltersFactory
+    {
+        public static SDK.ContentFilter CreateInstance(string classId, BuildContext context)
+        {
+            var filters = new Type[]
+            {
+                typeof(TestFilter1),
+                typeof(TestFilter2)
+            };
+
+            var t = filters.FirstOrDefault(item => item.Name == classId);
+
+            return t == null ? null : SDK.Create(t, context);
+        }
+
+        [SDK.ContentFilter("TestFilter1")]
+        class TestFilter1 : SDK.ContentFilter<int>
+        {
+            [SDK.InputValue("Value1")]
+            public int Value1 { get; set; }
+
+            [SDK.InputValue("Value2")]
+            public int Value2 { get; set; }
+
+            protected override int Evaluate()
+            {
+                return Value1 + Value2;
+            }
+        }
+
+        [SDK.ContentFilter("TestFilter2")]
+        class TestFilter2 : SDK.ContentFilter<int>
+        {
+            [SDK.InputNode("Value1")]
+            public int Value1 { get; set; }
+
+            [SDK.InputNode("Value2")]
+            public int Value2 { get; set; }
+
+            protected override int Evaluate()
+            {
+                return Value1 + Value2;
+            }
+        }
+
+    }
+
+
+    
+}
---
+++ src/Epsylon.UberFactory.Core.Tests/Tests.ProjectDOM.cs
@@ -0,0 +1,64 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Epsylon.UberFactory
+{
+    // https://www.visualstudio.com/en-us/docs/test/developer-testing/getting-started/getting-started-with-developer-testing
+
+    [TestClass]
+    public class ProjectDomTests
+    {
+        
+
+        [TestMethod]
+        public void DocumentFileLoadCheck()
+        {
+            var basicDocumentBody  = "<Project xmlns=\"http://www.uberfactory.com\" Version=\"1.0\"  > <Properties /> </Project>";
+            var futureDocumentBody = "<Project xmlns=\"http://www.uberfactory.com\" Version=\"999.0\"> <Properties /> </Project>";
+
+            Assert.IsNotNull(ProjectDOM.ParseProject(basicDocumentBody));            
+
+            Assert.ThrowsException<System.IO.FileLoadException> ( () => ProjectDOM.ParseProject(futureDocumentBody) );
+            
+        }
+
+        [TestMethod]
+        public void PropertyTests()
+        {
+            var pg = new ProjectDOM.PropertyGroup();
+
+            // null key check
+            Assert.ThrowsException<ArgumentNullException>(() => pg.SetValue(null, "X"));
+            Assert.ThrowsException<ArgumentNullException>(() => pg.SetValue(string.Empty, "X"));
+            Assert.ThrowsException<ArgumentNullException>(() => pg.SetValue("  ", "X"));
+            
+            Assert.IsFalse(pg.Contains("X"));
+            Assert.IsTrue(pg.SetArray("X", "A", "B", "C"));
+            Assert.IsFalse(pg.SetArray("X", "A", "B", "C"),"Failed to check nothing changed");            
+            Assert.AreEqual(3, pg.GetArray("X").Length);
+            CollectionAssert.AreEqual(pg.GetArray("X"), new string[] { "A", "B", "C" });
+            Assert.IsTrue(pg.Contains("X"));
+
+            Assert.IsTrue(pg.SetArray("X", "A"));
+            Assert.IsFalse(pg.SetArray("X", "A"));
+            Assert.AreEqual(1, pg.GetArray("X").Length);
+
+            pg.Clear("X");
+            Assert.IsFalse(pg.Contains("X"));
+        }
+
+        [TestMethod]
+        public void NodeTests()
+        {
+            var n = ProjectDOM.Node.Create("SomeFilter");
+
+            var pgroot = n.GetPropertiesForConfiguration("Root");
+            var pgchld = n.GetPropertiesForConfiguration("Root","Child");
+
+            // check property value inheritance
+            pgroot.SetValue("X", "A"); Assert.AreEqual("A", pgchld.GetValue("X", null));    
+            pgchld.SetValue("X", "B"); Assert.AreEqual("B", pgchld.GetValue("X", null));
+            pgchld.Clear("X");         Assert.AreEqual("A", pgchld.GetValue("X", null));
+        }
+    }
+}
