d1a0363ed869c32b13e48c286a38addec0164e2a
Vicente Penades <megaz0ne23@hotmail.com>
2017-05-28 00:56:15 +0200

    added test filters

---
+++ src/Epsylon.TestPlugins/Sinks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epsylon.TestPlugins
+{
+    using UberFactory;
+
+    [SDK.ContentFilter(nameof(TextSink))]
+    public sealed class TextSink : SDK.ContentFilter
+    {
+        [SDK.InputNode(nameof(Source))]
+        public String Source { get; set; }
+
+        protected override object EvaluateObject()
+        {
+            return null;
+        }
+    }    
+
+    [SDK.ContentFilter(nameof(IntegerSink))]
+    public sealed class IntegerSink : SDK.ContentFilter
+    {
+        [SDK.InputNode(nameof(Source))]
+        public Int32 Source { get; set; }
+
+        protected override object EvaluateObject()
+        {
+            return null;
+        }
+    }
+}
---
+++ src/Epsylon.TestPlugins/SpecialCaseFilters.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Epsylon.TestPlugins
+{
+    using UberFactory;
+
+    [SDK.ContentFilter(nameof(TestSlowProgressBar))]
+    public sealed class TestSlowProgressBar : SDK.ContentFilter<String>
+    {
+        protected override String Evaluate()
+        {
+
+            for (int i = 0; i < 100; ++i)
+            {
+                this.SetProgressPercent(i);
+                System.Threading.Thread.Sleep(50);
+            }
+
+            return string.Empty;
+        }
+    }
+
+    [SDK.ContentFilter(nameof(TestPipeline1))]
+    [SDK.ContentFilterMetaData("Title", "Debug Content Filter")]
+    public sealed class TestPipeline1 : SDK.ContentFilter<String>
+    {
+        [SDK.InputPipeline(nameof(Pipeline), typeof(string), typeof(string),typeof(string),typeof(string))]        
+
+        // Func<string,string,string,string>
+        public SDK.IPipelineInstance Pipeline { get; set; }
+
+        protected override String Evaluate()
+        {
+            return Pipeline.Evaluate("A", "B", "C") as String;
+        }
+    }
+
+    [SDK.ContentFilter(nameof(InvalidNodeTest))]
+    public sealed class InvalidNodeTest : SDK.ContentFilter<String>
+    {
+        [SDK.InputNode(nameof(InvalidType))]
+        public System.ResolveEventArgs InvalidType { get; set; }
+
+        [SDK.InputValue(nameof(Date))]
+        public DateTime Date { get; set; }
+
+        protected override String Evaluate() { return string.Empty; }
+    }
+}
