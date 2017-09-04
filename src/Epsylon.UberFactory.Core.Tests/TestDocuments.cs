00eb3dd20f4892f450ccd89eafa9a9a7c032cb01
Vicente Penades <megaz0ne23@hotmail.com>
2017-09-05 00:19:44 +0200

    improving unit tests

---
+++ src/Epsylon.UberFactory.Core.Tests/TestDocuments.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Epsylon.UberFactory
+{
+    [TestClass]
+    public class TestDocuments
+    {
+
+        [TestMethod]
+        public void TestInvalidDocuments()
+        {
+            // future documents           
+            Assert.ThrowsException<System.IO.FileLoadException>(() => _BuildDocument("Test.FutureDocument.uberfactory"));            
+
+            // missing plugin            
+            Assert.ThrowsException<InvalidOperationException>(() => _BuildDocument("Test.MissingPlugin.uberfactory"));
+
+            // malformed documents
+        }
+
+        [TestMethod]
+        public void CommandLineLoad()
+        {
+            var result = _BuildDocument("Test1.uberfactory");           
+
+            Assert.AreEqual("Root", result.ConfigurationJoined);            
+        }
+
+        private static Evaluation.BuildContext _BuildDocument(string docFileName)
+        {
+            docFileName = _GetAbsolutePath(docFileName);
+
+            Assert.IsFalse(string.IsNullOrWhiteSpace(docFileName));
+
+            var results = Evaluation.CommandLineContext.Build("-SIMULATE", "-CFG:Root", docFileName).ToArray();
+
+            Assert.AreEqual(1, results.Length);
+
+            return results[0];
+        }
+
+        private static string _GetAbsolutePath(string documentName)
+        {
+            var probeDir = Environment.CurrentDirectory;
+
+            while(probeDir.Length > 3)
+            {
+                var absPath = System.IO.Path.Combine(probeDir, documentName);
+                if (System.IO.File.Exists(absPath)) return absPath;
+
+                probeDir = System.IO.Path.GetDirectoryName(probeDir);
+            }
+
+            return null;
+        }
+    }
+}
--- src/Epsylon.UberFactory.Core.Tests/Tests.ProjectDOM.cs
+++ src/Epsylon.UberFactory.Core.Tests/Tests.ProjectDOM.cs
@@ -8,8 +8,6 @@ namespace Epsylon.UberFactory
     [TestClass]
     public class ProjectDomTests
     {
-        
-
         [TestMethod]
         public void DocumentFileLoadCheck()
         {
