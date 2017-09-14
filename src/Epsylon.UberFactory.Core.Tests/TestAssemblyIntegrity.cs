33b0866efbfa6aa345763d16df2d23e560122a51
Vicente Penades <megaz0ne23@hotmail.com>
2017-09-14 14:40:01 +0200

    WIP: assembly integrity tests WIP: auto version generator

---
+++ src/Epsylon.UberFactory.Core.Tests/TestAssemblyIntegrity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Epsylon.UberFactory
+{
+    [TestClass]
+    public class TestAssembly
+    {
+        // dump public API of an assembly
+        // https://code.msdn.microsoft.com/windowsdesktop/Code-Index-How-to-discover-98ba517b
+
+        // https://blogs.endjin.com/2016/02/an-experiment-to-automatically-detect-api-breaking-changes-in-dot-net-assemblies-and-suggest-a-semantic-version-number/
+
+        // Dot net API portability test
+        // https://github.com/Microsoft/dotnet-apiport
+
+        [TestMethod]
+        public void TestAssemblyVersion()
+        {
+            var SDK_Assembly = typeof(SDK.ContentFilter).Assembly;
+
+            Assert.IsTrue(SDK_Assembly.Version() >= new Version(1, 0, 0));
+
+            var infoVersion = SDK_Assembly.InformationalVersion();
+        }
+    }
+}
--- src/Epsylon.UberFactory.Core.Tests/_Extensions.cs
+++ src/Epsylon.UberFactory.Core.Tests/_Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Epsylon.UberFactory
@@ -53,6 +54,53 @@ namespace Epsylon.UberFactory
 
             return ids.Select(item => pipeline.GetNode(item));
         }
+
+        #region assemblies
+
+        public static Version Version(this Assembly assembly) { return assembly == null ? new Version() : assembly.GetName().Version; }
+
+        private static T _GetCustomAttribute<T>(this Assembly assembly) where T : Attribute { return Attribute.GetCustomAttribute(assembly, typeof(T), false) as T; }
+
+        public static string InfoCompany(this Assembly assembly) { return assembly._GetCustomAttribute<AssemblyCompanyAttribute>()?.Company; }
+
+        public static string InfoProductName(this Assembly assembly) { return assembly._GetCustomAttribute<AssemblyProductAttribute>()?.Product; }
+
+        public static string InformationalVersion(this Assembly assembly) { return assembly._GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion; }
+
+        public static string InfoCopyright(this Assembly assembly) { return assembly._GetCustomAttribute<AssemblyCopyrightAttribute>()?.Copyright; }
+
+        public static string DisplayTitle(this Assembly assembly) { return assembly == null ? null : assembly.InfoProductName() + " " + assembly.Version().ToString(); }
+
+        public static string GetDisplayTitle(this Assembly assembly, bool displayCompany, bool displayVersion, string currentDocument)
+        {
+            if (assembly == null) return null;
+            var title = assembly.InfoProductName();
+
+            if (displayCompany) title = assembly.InfoCompany() + " " + title;
+            if (displayVersion) title = title + " " + assembly.Version().ToString();
+
+            if (!string.IsNullOrWhiteSpace(currentDocument)) title = currentDocument.Trim() + " - " + title;
+
+            return title;
+        }
+
+        public static string GetMetadata(this Assembly assembly, string key)
+        {
+            var attributes = Attribute.GetCustomAttributes(assembly, typeof(AssemblyMetadataAttribute), true);
+            if (attributes == null) return null;
+
+            return attributes.OfType<AssemblyMetadataAttribute>().FirstOrDefault(item => item.Key == key)?.Value;
+        }
+
+        public static bool IsLoaded(this System.Diagnostics.FileVersionInfo fvinfo)
+        {
+            return AppDomain
+                    .CurrentDomain
+                    .GetAssemblies()
+                    .Any(a => string.Equals(a.Location, fvinfo.FileName, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        #endregion
 
     }
 }
