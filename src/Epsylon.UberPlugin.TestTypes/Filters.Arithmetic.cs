8cd0c835c7a40bd516efe08718a479d30991a561
Vicente Penades <megaz0ne23@hotmail.com>
2017-11-23 23:40:02 +0100

    added test plugin

---
+++ src/Epsylon.UberPlugin.TestTypes/Filters.Arithmetic.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epsylon.TestPlugins
+{
+    using UberFactory;    
+
+    [SDK.ContentNode(nameof(AssignIntegerValue))]
+    [SDK.Title("Value")]
+    public sealed class AssignIntegerValue : SDK.ContentFilter<int>
+    {
+        [SDK.InputValue(nameof(Value))]        
+        public int Value { get; set; }
+
+        protected override int Evaluate() => Value;
+    }
+
+    [SDK.ContentNode(nameof(AddIntegerValues))]
+    [SDK.Title("A + B")]
+    public sealed class AddIntegerValues : SDK.ContentFilter<int>
+    {
+        [SDK.InputNode(nameof(Value1))]
+        public int Value1 { get; set; }
+
+        [SDK.InputNode(nameof(Value2))]
+        public int Value2 { get; set; }
+
+        protected override int Evaluate() => Value1 + Value2;
+    }
+
+    [SDK.ContentNode(nameof(SubstractIntegerValues))]
+    [SDK.Title("A - B")]
+    public sealed class SubstractIntegerValues : SDK.ContentFilter<int>
+    {
+        [SDK.InputNode(nameof(Value1))]
+        public int Value1 { get; set; }
+
+        [SDK.InputNode(nameof(Value2))]
+        public int Value2 { get; set; }
+
+        protected override int Evaluate() => Value1 - Value2;
+    }
+
+    [SDK.ContentNode(nameof(MultiplyIntegerValues))]
+    [SDK.Title("A * B")]
+    public sealed class MultiplyIntegerValues : SDK.ContentFilter<int>
+    {
+        [SDK.InputNode(nameof(Value1))]
+        public int Value1 { get; set; }
+
+        [SDK.InputNode(nameof(Value2))]
+        public int Value2 { get; set; }
+
+        protected override int Evaluate() => Value1 * Value2;
+    }
+
+    [SDK.ContentNode(nameof(DivideIntegerValues))]
+    [SDK.Title("A / B")]
+    public sealed class DivideIntegerValues : SDK.ContentFilter<int>
+    {
+        [SDK.InputNode(nameof(Value1))]
+        public int Value1 { get; set; }
+
+        [SDK.InputNode(nameof(Value2))]
+        public int Value2 { get; set; }
+
+        protected override int Evaluate() => Value1 / Value2;
+    }
+
+
+
+
+
+
+}
---
+++ src/Epsylon.UberPlugin.TestTypes/Filters.SpecialCases.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Epsylon.TestPlugins
+{
+    using UberFactory;
+
+    [SDK.ContentNode(nameof(AllTypesFilter))]
+    public sealed class AllTypesFilter : SDK.ContentFilter<String>
+    {
+        [SDK.InputValue(nameof(Time))]
+        public TimeSpan Time { get; set; }
+
+        protected override String Evaluate()
+        {
+            return string.Empty;
+        }
+    }
+
+    [SDK.ContentNode(nameof(TestSlowProgressBar))]
+    public sealed class TestSlowProgressBar : SDK.ContentFilter<String>
+    {
+        [SDK.InputValue(nameof(PauseTime))]
+        [SDK.Title("Pause Time in Seconds")]
+        [SDK.Default(10), SDK.Minimum(1)]        
+        public int PauseTime { get; set; }
+
+        protected override String Evaluate()
+        {
+            var waitTime = TimeSpan.FromSeconds( (double)PauseTime / 100 );
+
+            for (int i = 0; i < 100; ++i)
+            {
+                this.SetProgressPercent(i);
+
+                System.Threading.Tasks.Task.Delay(waitTime).Wait();
+            }
+
+            return string.Empty;
+        }
+    }
+        
+    [SDK.ContentNode(nameof(InvalidNodeTest))]
+    public sealed class InvalidNodeTest : SDK.ContentFilter<String>
+    {
+        /// <summary>
+        /// We've chosen a return type that it's going to be rarely used as a interoperation type to throw
+        /// </summary>
+        [SDK.InputNode(nameof(InvalidType))]
+        public System.Attribute InvalidType { get; set; }
+
+        [SDK.InputValue(nameof(InvalidArray))]
+        public int[][,] InvalidArray { get; set; }
+
+        [SDK.InputValue(nameof(Date))]
+        public DateTime Date { get; set; }
+
+        protected override String Evaluate() { return string.Empty; }
+    }
+}
