1868dad39a771b78d7bba4b8398845e86490629c
Vicente Penades <megaz0ne23@hotmail.com>
2017-12-18 17:34:24 +0100

    Added copy/passthrough filter

---
+++ src/Epsylon.UberPlugin.CoreTypes/CoreFilters.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Epsylon.UberPlugin
+{
+    using UberFactory;
+
+    [SDK.ContentNode("FileCopy")]
+    [SDK.Title("Copy File")]
+    public class FileCopy : SDK.ContentFilter
+    {
+        [SDK.InputValue("Enabled")]
+        [SDK.Title("Enabled")]
+        [SDK.Default(true)]
+        public Boolean Enabled { get; set; }
+
+        [SDK.InputValue("SourceFileName")]
+        [SDK.Title("Source File")]
+        [SDK.ViewStyle("FilePicker")]
+        [SDK.MetaData("Filter", "*.*")]
+        public String SourceFileName { get; set; }
+
+        [SDK.InputValue("TargetFileName")]
+        [SDK.Title("Target File")]
+        public String TargetFileName { get; set; }
+
+        protected override object EvaluateObject()
+        {
+            if (!Enabled) return null;
+
+            var inPath = SourceFileName;
+            var outPath = string.IsNullOrWhiteSpace(TargetFileName) ? System.IO.Path.GetFileName(inPath) : TargetFileName;
+
+            var inCtx = this.GetImportContext(inPath);
+            if (inCtx == null) throw new System.IO.FileNotFoundException("Error opening file", inPath.ToString());
+
+            var outCtx = this.GetExportContext(outPath);
+            if (outCtx == null) return null;
+
+            var bytes = inCtx.ReadAllBytes();
+
+            outCtx.WriteAllBytes(bytes);
+
+            return bytes;
+        }
+    }
+}
