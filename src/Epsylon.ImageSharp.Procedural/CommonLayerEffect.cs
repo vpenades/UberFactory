f0a3cd1c49a2577e0a4a28f12b18ffb3ba90f510
Vicente Penades <megaz0ne23@hotmail.com>
2018-01-31 17:33:24 +0100

    comment

--- src/Epsylon.ImageSharp.Procedural/CommonLayerEffect.cs
+++ src/Epsylon.ImageSharp.Procedural/CommonLayerEffect.cs
@@ -8,8 +8,10 @@ using SixLabors.Primitives;
 
 namespace Epsylon.ImageSharp.Procedural
 {
-    
-    
+    // https://github.com/KDE/krita/tree/f352cc1d4367542ad55d61636927e90c56e92b7d/libs/image/layerstyles
+    // http://registry.gimp.org/node/186
+
+
     public class CommonEffect<TPixel> where TPixel : struct, IPixel<TPixel>
     {
         #region data
